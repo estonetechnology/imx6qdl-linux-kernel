d5069193445995bbd30a7e4e04fbba0f4a996ad8
wangwenjing <wangwenjing@shencloudtech.com>
2015-02-03 10:23:52 +0800

    fixed the compile configure bug

--- include/linux/SHEN_TYPE.h
+++ include/linux/SHEN_TYPE.h
@@ -3,9 +3,10 @@
 
 //Type index list
 #define TYPE_VKI               1
-#define TYPE_RII               2
+#define TYPE_RRI_WIFI          2
+#define TYPE_RRI_WIFI_BT       3
 
-#define CUR_SHEN_TYPE TYPE_RII
+#define CUR_SHEN_TYPE TYPE_RRI_WIFI
 
 #endif
 
